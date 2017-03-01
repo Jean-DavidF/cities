385638014187f57302a8cba9cfe9c114ef25b5a2
Jean-David Flament <jean-david.flament@102f14.iut-lens.univ-artois.fr>
2017-03-01 21:57:12 +0100

    MENU OK !!

--- client/navbar/navbar.js
+++ client/navbar/navbar.js
@@ -1,6 +1,14 @@
 Template.navbar.events({
     'click #reset' : function() {
         Meteor.call('reset');
-        alert('OK');
+    }
+});
+
+Template.navbar.events({
+    'click #burger-btn': function (e) {
+        $("#menu-open").removeClass('displaynone');
+    },
+    'click #cross-icon': function (e) {
+        $("#menu-open").addClass('displaynone');
     }
 });
