8b197808a5b120f493a066e52157fef97c2670ba
Jean-David Flament <jean-david.flament@102f14.iut-lens.univ-artois.fr>
2017-03-01 16:56:15 +0100

    Not Found upadte and cities

--- client/routes.js
+++ client/routes.js
@@ -11,12 +11,11 @@ Router.route("/choice", {
     template: "choice"
 });
 
-Router.route("/city/:id", {
-    template: "city",
-    data : function() {
-        console.log(this.params.id);
-        console.log(Cities.findOne({_id : this.params.id}));
-        return Cities.findOne({_id : this.params.id});
-    }
+Router.route("/city/:id", function() {
+    var city = Cities.findOne({_id : this.params.id})
+    if(typeof city == "undefined") 
+        this.render("notFound");
+    else
+        this.render("city",{data : city});
 });
 
