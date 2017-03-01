db2a3c1152878d848aaca8b4019b820746945c39
Jean-David Flament <jean-david.flament@102f14.iut-lens.univ-artois.fr>
2017-03-01 16:26:17 +0100

    update

--- client/routes.js
+++ client/routes.js
@@ -19,15 +19,4 @@ Router.route("/city/:id", {
         return Cities.findOne({_id : this.params.id});
     }
 });
-<<<<<<< HEAD
-/*
-Router.route('/city/:id', {
-    template: "city",
-    data: function () {
-        // Return the document for the selected city (the one whose id is given)
-        // The value of this id is given by  this.params.id
-    }
-});
-*/
-=======
->>>>>>> 6c66b0a344f5c3944d5fbf418980684d6fb142b2
+
