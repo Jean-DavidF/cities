0d4af5025bc4e2b5c5e592a065ad346a876857a3
Jean-David Flament <jean-david.flament@102f14.iut-lens.univ-artois.fr>
2017-03-01 14:05:13 +0100

    choice page current changes (version before class)

--- client/choice/choice.js
+++ client/choice/choice.js
@@ -5,4 +5,16 @@ Template.choice.helpers({
     "city2": function(){
         return Cities.findOne({_id : "c1"});
     }
-})
+});
+
+$("#city-left").mouseover(function(){
+    $("#description-city1").css({
+        opacity: 1,
+    });
+});
+
+$("#city-right").mouseover(function(){
+    $("#description-city2").css({
+        opacity: 1,
+    });
+});
