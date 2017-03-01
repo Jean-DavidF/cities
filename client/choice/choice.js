c98f642b34062cdb343728efc631736a4163a79d
Jean-David Flament <jean-david.flament@102f14.iut-lens.univ-artois.fr>
2017-03-01 16:06:14 +0100

    choice page ok (final)

--- client/choice/choice.js
+++ client/choice/choice.js
@@ -7,40 +7,25 @@ Template.choice.helpers({
     }
 });
 
-/*
-$(document).ready(function(){
-$("#description-city2").mouseenter(function(){
-    $("#description-city1").css({
-        opacity: 1,
-    });
-});
-
-$("#description-city1").mouseenter(function(){
-    $("#description-city2").css({
-        opacity: 1,
-    }););
-});
-}); */
-
 Template.choice.events({
     'mouseenter #description-city1': function (e) {
         $("#description-city2").animate({
             opacity: 1,
-        }),75;
+        },100);
     },
     'mouseenter #description-city2': function (e) {
         $("#description-city1").animate({
             opacity: 1,
-        }),75;
+        },100);
     },
     'mouseleave #description-city1': function (e) {
-        $("#description-city2").css({
+        $("#description-city2").animate({
             opacity: 0.01,
-        });
+        },100);
     },
     'mouseleave #description-city2': function (e) {
-        $("#description-city1").css({
+        $("#description-city1").animate({
             opacity: 0.01,
-        });
+        },100);
     }
 });
